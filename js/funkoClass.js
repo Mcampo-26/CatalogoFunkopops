37cf23adb1d9a5005a18a116aa4bb9a262251dc3
Mauricio Campo <mcampo26@gmail.com>
2021-04-21 08:49:20 -0500

    realizo el maquetado y agreagamos la clase funko

---
+++ js/funkoClass.js
@@ -0,0 +1,10 @@
+export class Funko {
+    constructor(codigoP, nombreP, numSerieP, categoriaP, descripcionP, imagenP) {
+        this.codigo = codigoP;
+        this.nombre = nombreP;
+        this.numSerie = numSerieP;
+        this.descripcion = descripcionP;
+        this.imagen = imagenP;
+
+    }
+}
