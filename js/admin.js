df98b10896627501ff01ecc1d0d6ba6ddd43156e
Mauricio Campo <mcampo26@gmail.com>
2021-04-25 20:50:06 -0500

    se agrego la logica para agregar un funkopop

--- js/admin.js
+++ js/admin.js
@@ -1,5 +1,13 @@
 import { Funko } from "./funkoClass.js";
-let listaFunkopop = []
+let listaFunkopop = [];
+const modalFunko = new bootstrap.Modal(document.getElementById('modalProducto'));
+
+//funcion agergar funkopop (event){}
+let btnAgregar = document.getElementById("btnAgregar");
+btnAgregar.addEventListener("click", () => {
+    modalFunko.show();
+})
+
 window.agregarFunkopop = function(event) {
     //el objetico de esta funcin es agegar un funkopop nuevo en local storage
     event.preventDefault();
@@ -19,4 +27,29 @@ window.agregarFunkopop = function(event) {
     // agregar el nuevo objeto en el arreglo de funkopop
     listaFunkopop.push(nuevoFunkopop);
     console.log(listaFunkopop);
+    // guardar datos en localstorage
+    localStorage.setItem("listaFunkoKey", JSON.stringify(listaFunkopop));
+    limpiarFormulario();
+    //mostrar un mensaje al usuario
+    Swal.fire(
+            'Good job!',
+            'You clicked the button!',
+            'success'
+        ) //mostrar un mensaje al usuario
+    Swal.fire(
+            'Nuevo producto',
+            'El Funkopop se agrego correctamente',
+            'success'
+        )
+        //cerrar ventana modal 
+
+    modalFunko.hide()
+
+};
+// limpiar el formulario
+function limpiarFormulario() {
+    //aquie estamos resteando lo svalores del formulario
+    let formulario = document.getElementById("formFunkopop");
+    formulario.reset();
 }
+// cerrar a ventana modal
