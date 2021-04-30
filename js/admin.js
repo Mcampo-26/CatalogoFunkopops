cb99779e170a104d76f8640b1c1de177567a6db4
Mauricio Campo <mcampo26@gmail.com>
2021-04-30 12:35:12 -0500

    agregamos la funcion de elimiar funopopp, y agregamos la funcion preparar funkopop

--- js/admin.js
+++ js/admin.js
@@ -1,12 +1,19 @@
 import { Funko } from "./funkoClass.js";
 let listaFunkopop = [];
-const modalFunko = new bootstrap.Modal(document.getElementById('modalProducto'));
-
+const modalFunko = new bootstrap.Modal(
+    document.getElementById('modalProducto')
+);
+//modificarFunko=true quiero modificar un funko existente
+//modificarFunko=false quiero agregar un nuevo funko 
+let modificarFunko = false;
 //funcion agergar funkopop (event){}
 let btnAgregar = document.getElementById("btnAgregar");
 btnAgregar.addEventListener("click", () => {
-    modalFunko.show();
-})
+        //mostrar ventana modal
+        modalFunko.show();
+    })
+    // buscar los datos del local storage
+leerDatos();
 
 window.agregarFunkopop = function(event) {
     //el objetico de esta funcin es agegar un funkopop nuevo en local storage
@@ -14,14 +21,12 @@ window.agregarFunkopop = function(event) {
     console.log("estamos dentro de la funcion funkopop")
         // traer los valores del formulario que ya estan validados
 
-
     let codigo = document.getElementById("codigo").value;
     let nombre = document.getElementById("nombre").value;
     let numSerie = document.getElementById("numSerie").value;
     let categoria = document.getElementById("categoria").value;
     let descripcion = document.getElementById("descripcion").value;
     let imagen = document.getElementById("imagen").value;
-
 
     let nuevoFunkopop = new Funko(codigo, nombre, numSerie, categoria, descripcion, imagen);
     // agregar el nuevo objeto en el arreglo de funkopop
@@ -44,7 +49,6 @@ window.agregarFunkopop = function(event) {
         //cerrar ventana modal 
 
     modalFunko.hide()
-
 };
 // limpiar el formulario
 function limpiarFormulario() {
@@ -52,4 +56,94 @@ function limpiarFormulario() {
     let formulario = document.getElementById("formFunkopop");
     formulario.reset();
 }
-// cerrar a ventana modal
+// leer datos del local storage
+function leerDatos() {
+    if (localStorage.length > 0) {
+        //traer datos del local storage
+        let listaFunkopop_ls = JSON.parse(localStorage.getItem("listaFunkoKey"));
+
+        if (listaFunkopop.length === 0) {
+            listaFunkopop = listaFunkopop_ls;
+        }
+        // funcion dibujar los datos en una tabla
+        dibujarTabla(listaFunkopop_ls);
+    }
+}
+
+function dibujarTabla(listaFunkopop_ls) {
+    // traer el tbody
+    let tablaFunko = document.getElementById("tablaFunko");
+    //variable para trabajar codigo html, donde voy a crear las filas 
+    let filaFunko = "";
+    //limpia los datos del tbody
+    tablaFunko.innerHTML = "";
+    //for (let i=0; i< listaFunkopop_ls.length; i++){}
+    for (let i in listaFunkopop_ls) {
+        // for in, hace solo el length sin escribirlo  
+        // crear en al fila
+        filaFunko = `<tr>
+        <th>${listaFunkopop_ls[i].codigo}</th>
+        <td>${listaFunkopop_ls[i].nombre}</td>
+        <td>${listaFunkopop_ls[i].numSerie}</td>
+        <td>${listaFunkopop_ls[i].categoria}</td>
+        <td>${listaFunkopop_ls[i].descripcion}</td>
+        <td>${listaFunkopop_ls[i].imagen}</td>
+        <td>
+            <button class=" btn btn-warning" onclick='preparDatosFunko(this)'id='${listaFunkopop_ls[i].codigo}'>Editar</button>
+            <button class=" btn btn-danger" onclick ="eliminarFunkopop(this)" id="${listaFunkopop_ls[i].codigo}"
+         >Borrar</button>
+        </td>
+    </tr>`
+            //agregar la fila al elemento padre
+        tablaFunko.innerHTML += filaFunko;
+    }
+}
+window.eliminarFunkopop = function(boton) {
+    console.log(boton.id);
+    Swal.fire({
+        title: 'Esta seguro de eliminar el funkopop?',
+        text: "No puedes volver atras luego de este paso",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'si',
+        cancelButtonText: 'cancelar'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            // aca va el codigo para eliminar el fubkopop, .Filter es para filtrar lo que hay dento del arreglo como por ej su codigo, nombre etc..
+            let funkopopFiltrados = listaFunkopop.filter(function(producto) {
+                    return producto.codigo != boton.id
+                })
+                //eso en forma de flecha seria //let funkopopFiltrados = listaFunkopop.filter(producto =>producto.codigo != boton.id)
+            console.log(funkopopFiltrados);
+            // igualar los arreglos
+            listaFunkopop = funkopopFiltrados
+                // guardar los datos en localstorage
+            localStorage.setItem("listaFunkoKey", JSON.stringify(listaFunkopop))
+                //llamar a la funcion leer datos
+            leerDatos();
+
+            Swal.fire(
+                'eliminado',
+                'no puedes volver atras luego de este paso.',
+                'success'
+            )
+        }
+    })
+}
+window.preparDatosFunko = function(boton) {
+    console.log(boton.id)
+    let funkoEncontrado = listaFunkopop.find(function(producto) {
+        return producto.codigo === boton.id;
+    })
+    console.log(funkoEncontrado);
+    document.getElementById('codigo').value = funkoEncontrado.codigo;
+    document.getElementById('nombre').value = funkoEncontrado.nombre;
+    document.getElementById('numSerie').value = funkoEncontrado.numSerie;
+    document.getElementById('categoria').value = funkoEncontrado.categoria;
+    document.getElementById('descripcion').value = funkoEncontrado.descripcion;
+    document.getElementById('imagen').value = funkoEncontrado.imagen;
+    modalFunko.show();
+
+}
